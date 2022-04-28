ed819ac502714270c78d5d1dd377e242bbdc0b3d
Gabriel Sousa Leal <gabriel.leal@safra.com.br>
2022-04-28 19:12:50 -0300

    organizando melhor o codigo

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const request = require('request');
 const fs = require('fs');
 const axios = require('axios');
+const { url } = require('inspector');
 
 let jsonData = fs.readFileSync("./config.json", "utf8");
 let data = JSON.parse(jsonData)
+var access_token = ""
 main();
 
 /**
@@ -17,57 +19,98 @@ main();
 
     let email = data['email'];
     let password = data['password'];
-	let formation = data['formation']
+	let formationName = data['formation']
     
-    let access_token = await sign_in(email, password);
+	 access_token = await sign_in(email, password);
 
     if (!access_token) {
     	return;
     }
 
-	let parse = await get_formation(access_token, formation);
-	let formationJson = JSON.parse(parse);
-	let steps = formationJson.steps;
-	let formationTitle = tratarTitulo(formation)
-	create_folder(formationTitle)
-	let courses = []
+	let formation = await fetchFormation(formationName)
 
-	for (let i = 0; i < steps.length; i++) {
-		let currentStep = steps[i]
-		let stepCourses = currentStep.courses
-		let currentStepTitle = tratarTitulo(currentStep.title)
-		create_folder(`${formationTitle}/${currentStepTitle}`)
-		for (let j = 0; j < stepCourses.length; j++) {
-			let currentCourse = stepCourses[j]
-            let courseSlug = currentCourse.slug
-			let currentCourseTitle = tratarTitulo(currentCourse.title)
-			create_folder(`${formationTitle}/${currentStepTitle}/${currentCourseTitle}`)
-            let courseResponse = await get_course(access_token, courseSlug);
-			let course = JSON.parse(courseResponse)
-			let sections = course.sections
-			for (let k = 0; k < sections.length; k++) {
-				let videos = sections[k].videos
-				for (let l = 0; l < videos.length; l++) {
-					let currentVideo = videos[l]
-					let currentVideoTitle = tratarTitulo(currentVideo.nome)
-					let videoID = currentVideo.id
-					let folderName = `${formationTitle}/${currentStepTitle}/${currentCourseTitle}/${currentVideoTitle}`
-					let url = await get_video(videoID, courseSlug, access_token);
-					video_download(`${folderName}.mp4`, url, currentVideoTitle)
-				}
-			}
-		}
-	}
+	formation.steps.forEach( async step => {
+		await getCoursesFromStep(step, formation.title)
+	})
 }
 
-function downloadVideo() {
-   
+async function fetchFormation(formationName) {
+	let parse = await get_formation(formationName);
+	let formation = JSON.parse(parse);
+	return formation
+}
+
+async function getCoursesFromStep(step, formation) {
+	step.courses.forEach( async course => {
+		let slug = course.slug
+		enterCourse(slug)
+		let completeCourse = await getCompleteCourse(slug)
+		let folder = `${formation}/${step.title}/${course.title}`
+		completeCourse.sections.forEach( section => {
+			downloadVideosFromSection(section, slug, folder)
+		})
+	})
+}
+
+async function getCompleteCourse(slug) {
+	let completeCourse = await get_course(slug)
+	return JSON.parse(completeCourse)
+}
+
+ async function downloadVideosFromSection(section, courseSlug, folder) {
+	let videos = section.videos
+	videos.forEach(async video => { 
+		let currentVideoTitle = tratarTitulo(video.nome)
+		let videoID = video.id
+		let createdFolder = create_folder(folder)
+		let videoPath = `${createdFolder}/${currentVideoTitle}.mp4`
+		let url = await get_video(videoID, courseSlug, access_token)
+		video_download(videoPath, url)
+	});
 }
 
 function tratarTitulo(titulo) {
 	var textoSemAcentos = titulo.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 	var textoTratado = textoSemAcentos.replace(/[^\w\s]/gi, '');
 	return textoTratado;
+}
+
+/**
+ * get link video for download
+ * @param {string} slug 
+ */
+async function enterCourse(slug) {
+	let res = await http_request({
+		url: `https://cursos.alura.com.br/courses/${slug}/tryToEnroll`,
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'User-Agent': 'alura-mobi/android',
+			'Host': 'cursos.alura.com.br',
+			'Authorization': `Bearer ${access_token}`,
+			'Connection': 'Keep-Alive'
+		}
+	})
+}
+
+/**
+ * get link video for download
+ * @param {string} slug 
+ * @param {string} token 
+ */
+ async function stopCourse(slug, token) {
+	let res = await http_request({
+		url: `https://cursos.alura.com.br/courses/stop`,
+		method: 'POST',
+		body: `registrationId=8497089`,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'User-Agent': 'alura-mobi/android',
+			'Host': 'cursos.alura.com.br',
+			'Authorization': `Bearer ${token}`,
+			'Connection': 'Keep-Alive'
+		}
+	})
 }
 
 /**
@@ -123,7 +166,7 @@ function tratarTitulo(titulo) {
  * @param {sting} access_token 
  * @param {string} formation 
  */
-  async function get_formation(access_token, formation) {
+  async function get_formation(formation) {
 
 	let res = await http_request({
 		url: `https://www.alura.com.br/api/${formation}`,
@@ -145,8 +188,7 @@ function tratarTitulo(titulo) {
  * @param {sting} access_token 
  * @param {string} course 
  */
- async function get_course(access_token, course) {
-
+ async function get_course(course) {
  	let res = await http_request({
  		url: `https://cursos.alura.com.br/mobile/v2/course/${course}`,
  		headers: {
@@ -159,7 +201,6 @@ function tratarTitulo(titulo) {
  	})
 
  	return res.body
-
  }
 
 /**
@@ -167,8 +208,7 @@ function tratarTitulo(titulo) {
  * @param {string} path 
  * @param {string} url 
  */
- async function video_download(path, url, title) {
-	console.log(path)
+ async function video_download(path, url) {
  	const response = await axios({
  		method: 'GET',
  		url: url,
@@ -200,8 +240,15 @@ function tratarTitulo(titulo) {
  * @param {string} dir 
  */
  function create_folder(dir) {
- 	if (!fs.existsSync(__dirname + '/' + dir)) {
- 		fs.mkdirSync(__dirname + '/' + dir);
- 	}
+	 let folders = dir.split('/')
+	 var currentFolder = ""
+	 folders.forEach( folder => {
+		let folderTratado = tratarTitulo(folder)
+		currentFolder += `${folderTratado}/`
+		if (!fs.existsSync(__dirname + '/' + currentFolder)) {
+			fs.mkdirSync(__dirname + '/' + currentFolder);	
+		}
+	 })
+	 return currentFolder
  }
 
