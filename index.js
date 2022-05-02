b1774a31e7a3cdd71ba8b153270c7708d18ba72a
Gabriel Sousa Leal <gabriel.leal@safra.com.br>
2022-05-02 10:35:09 -0300

    downloadFull

--- index.js
+++ index.js
@@ -4,23 +4,63 @@ const fs = require('fs');
 let jsonData = fs.readFileSync("./config.json", "utf8");
 let data = JSON.parse(jsonData)
 let services = require('./modules/services.js')
+var errorVideoList = []
+var errorFormationList = []
 main();
 
  async function main() {
     let email = data['email'];
     let password = data['password'];
 	let formationList = data['formations']
-    
-	let logged = await services.login(email, password);
-	if (!logged) { return }
+	let downloadAllFormations = data['downloadAllFormations']
 
+	services.login(email, password).then(logged => {
+		if (!logged) { 
+			console.log('Falha no Login')
+			return 
+		}
+	
+		if (downloadAllFormations) {
+			downloadFromAllFormations()
+		} else {
+			downloadFromFormationList(formationList)
+		}
+	}) 
+
+	// console.log('FORMACOES QUE DERAM ERRO: ' + errorFormationList)
+	// console.log('VIDEOS QUE DEAM ERRO: ' + errorVideoList)
+}
+
+async function downloadFromFormationList(formationList) {
 	let formationNames = getFormationsNames(formationList)
 
 	formationNames.forEach( async formationName => {
-		console.log(formationName)
-		let formation = await fetchFormation(formationName)
-		formation.steps.forEach( async (step, index) => {
-			await getCoursesFromStep(step, formation.title, index)
+		try {
+			fetchFormation(formationName)
+			let formationCategory = formation.categoryName
+			formation.steps.forEach( async (step, index) => {
+				getCoursesFromStep(step, formation.title, index, formationCategory)
+			})
+		} catch {
+			errorFormationList.push(formationName)
+		}
+	})
+}
+
+async function downloadFromAllFormations() {
+	services.fetchAllFormations().then(formations => {
+		formations.forEach( async formation => {
+			try {
+				let formationCategory = formation.categoryName
+				var name = 'formacao-' + formation.code
+				fetchFormation(name).then(completeFormation => {
+					completeFormation.steps.forEach( async (step, index) => {
+						getCoursesFromStep(step, formation.title, index, formationCategory)
+					})
+				})
+			} catch {
+				errorFormationList.push(formationName)
+			}
 		})
 	})
 }
@@ -30,45 +70,56 @@ function getFormationsNames(formationList) {
 	formationList.forEach( formation => {
 		let splited = formation.split('/')
 		let name = splited[splited.length - 1]
-		console.log(name)
 		names.push(name)
 	});
 	return names
 }
 
 async function fetchFormation(formationName) {
-	let parse = await services.fetchFormation(formationName);
-	let formation = JSON.parse(parse);
-	return formation
+	services.fetchFormation(formationName).then(parse => {
+		try {
+			let formation = JSON.parse(parse);
+			return formation
+		} catch {
+			console.log('Erro ao baixar formação ' + formationName)
+			throw 'erro'
+		}
+	})
 }
 
-async function getCoursesFromStep(step, formation, stepIndex) {
+async function getCoursesFromStep(step, formation, stepIndex, formationCategory) {
 	step.courses.forEach( async (course, coursesIndex) => {
 		let slug = course.slug
 		services.enterCourse(slug)
-		let completeCourse = await getCompleteCourse(slug)
-		completeCourse.sections.forEach( async (section, index) => {
-			let folder = `${formation}/${stepIndex + 1} ${step.title}/${coursesIndex + 1} ${course.title}/ ${index + 1} - ${section.titulo}`
-			downloadVideosFromSection(section, slug, folder)
+		getCompleteCourse(slug).then(completeCourse => {
+			completeCourse.sections.forEach( async (section, index) => {
+				let folder = `${formationCategory}/${formation}/${stepIndex + 1} ${step.title}/${coursesIndex + 1} ${course.title}/ ${index + 1} - ${section.titulo}`
+				downloadVideosFromSection(section, slug, folder)
+			})
 		})
 	})
 }
 
 async function getCompleteCourse(slug) {
-	let completeCourse = await services.fetchCourse(slug)
-	return JSON.parse(completeCourse)
+	services.fetchCourse(slug).then(completeCourse => {
+		return JSON.parse(completeCourse)
+	})
 }
 
  async function downloadVideosFromSection(section, courseSlug, folder) {
 	let videos = section.videos
-	console.log(videos.length)
 	videos.forEach(async (video, index) => { 
 		let currentVideoTitle = removeSpecialCharacters(video.nome)
 		let videoID = video.id
 		let createdFolder = createFolder(folder)
 		let videoPath = `${createdFolder}/${index + 1} ${currentVideoTitle}.mp4`
-		let url = await services.fetchVideo(videoID, courseSlug)
-		services.downloadVideo(videoPath, url)
+		try {
+			services.fetchVideo(videoID, courseSlug).then(url => {
+				services.downloadVideo(videoPath, url)
+			})
+		} catch {
+			errorVideoList.push(formationName)
+		}
 	});
 }
 
@@ -90,3 +141,11 @@ function removeSpecialCharacters(string) {
 	 })
 	 return currentFolder
  }
+
+ String.prototype.insert = function(index, string) {
+	if (index > 0) {
+	  return this.substring(0, index) + string + this.substr(index);
+	}
+  
+	return string + this;
+  };
--- modules/Services.js
+++ modules/Services.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 exports.login = login
 exports.enterCourse = enterCourse
 exports.fetchFormation = fetchFormation
+exports.fetchAllFormations = fetchAllFormations
 exports.fetchVideo = fetchVideo
 exports.fetchCourse = fetchCourse
 exports.downloadVideo = downloadVideo
@@ -16,7 +17,8 @@ function fetch(options) {
  }
 
 async function enterCourse(slug) {
-	let res = await fetch({
+    console.log('Entrando no curso ' + slug)
+	fetch({
 		url: `https://cursos.alura.com.br/courses/${slug}/tryToEnroll`,
 		method: 'GET',
 		headers: {
@@ -26,11 +28,19 @@ async function enterCourse(slug) {
 			'Authorization': `Bearer ${accessToken}`,
 			'Connection': 'Keep-Alive'
 		}
-	})
+	}).then(res => {
+        if (res.error) {
+            console.log('Erro ao entrar no curso ' + res.error)
+        } else {
+            console.log('Entrou no curso ' + slug)
+        }
+        return
+    })
 }
 
  async function login(mail, pass) {
-    let res = await fetch({
+     console.log('Fazendo login')
+    fetch({
         url: 'https://cursos.alura.com.br/mobile/token',
         method: 'POST',
         body: `password=${pass}&client_secret=3de44ac5f5bccbcfba14a77181fbdbb9&client_id=br.com.alura.mobi&username=${mail}&grant_type=password`,
@@ -40,18 +50,25 @@ async function enterCourse(slug) {
             'Host': 'cursos.alura.com.br',
             'Connection': 'Keep-Alive'
         }
+    }).then(res => {
+        console.log(res.body)
+        if (res.error) {
+            console.log('Erro ao logar ' + res.error)
+        }
+    
+        if (res.body.includes('access_token')){
+            let token = JSON.parse(res.body).access_token;
+            accessToken = token
+            console.log('Login feito!')
+            return true
+        } 
+        return false
     })
-
-    if (res.body.includes('access_token')){
-        let token = JSON.parse(res.body).access_token;
-        accessToken = token
-        return true
-    } 
-    return false
 }
 
  async function fetchVideo(id, slug) {
-    let res = await fetch({
+     console.log('Baixando informacoes sobre o video ' + id)
+    fetch({
         url: `https://cursos.alura.com.br/mobile/courses/${slug}/busca-video-${id}`,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -60,15 +77,45 @@ async function enterCourse(slug) {
             'Authorization': `Bearer ${accessToken}`,
             'Connection': 'Keep-Alive'
         }
-    });
+    }).then(res => {
+        if (res.error) {
+            console.log('Erro ao baixar video ' + slug + ' ' + res.error)
+        }
+    
+        try {
+            let [hd, sd] = JSON.parse(res.body);
+            return hd.link;
+        } catch {
+            throw res.error
+        }
+    })
+}
 
-    let [hd, sd] = JSON.parse(res.body);
-    return hd.link;
+async function fetchAllFormations() {
+    console.log('Baixando informações sobre todas as formações, aguarde.....')
+	fetch({
+		url: `https://cursos.alura.com.br/api/formacoes`,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'User-Agent': 'alura-mobi/android',
+			'Host': 'cursos.alura.com.br',
+			'Authorization': `Bearer ${accessToken}`,
+			'Connection': 'Keep-Alive'
+		}
+	}).then(res => {
+        if (res.error) {
+            console.log('Erro ao obter informacoes de formacoes ' + res.error)
+        } else {
+            console.log('Informaçoes sobre todas as formacoes baixadas')
+        }
+    
+        return JSON.parse(res.body)
+    })
 }
 
  async function fetchFormation(formation) {
-
-	let res = await fetch({
+    console.log('Baixando informacoes sobre a formacao ' + formation)
+	fetch({
 		url: `https://www.alura.com.br/api/${formation}`,
 		headers: {
 			'Content-Type': 'application/x-www-form-urlencoded',
@@ -77,14 +124,20 @@ async function enterCourse(slug) {
 			'Authorization': `Bearer ${accessToken}`,
 			'Connection': 'Keep-Alive'
 		}
-	})
-
-	return res.body
-
+	}).then(res => {
+        if (res.error) {
+            console.log('Erro ao baixar formacao ' + res.error)
+        } else {
+            console.log('Informacoes sobre a formacao baixada')
+        }
+    
+        return res.body
+    })
 }
 
 async function fetchCourse(course) {
-    let res = await fetch({
+    console.log('Baixando informacao sobre o curso ' + course)
+    fetch({
         url: `https://cursos.alura.com.br/mobile/v2/course/${course}`,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -93,13 +146,20 @@ async function fetchCourse(course) {
             'Authorization': `Bearer ${accessToken}`,
             'Connection': 'Keep-Alive'
         }
+    }).then(res => {
+        if (res.error) {
+            console.log('Erro ao baixar informacoes do curso ' + res.error)
+        } else {
+            console.log('Informacoes do curso baixadas')
+        }
+    
+        return res.body
     })
-
-    return res.body
 }
 
 async function downloadVideo(path, url) {
-    console.log('baixando: ' + path)
+    console.log(url)
+    console.log('Baixando: ' + path)
     const response = await axios({
         method: 'GET',
         url: url,
@@ -109,6 +169,7 @@ async function downloadVideo(path, url) {
     response.data.pipe(fs.createWriteStream(path))
     return new Promise((resolve, reject) => {
         response.data.on('end', () => {
+
             resolve()
         })
 
